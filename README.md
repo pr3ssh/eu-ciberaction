f05f3a492e4aea05dd5ca79d5ed1027a96e2ed77
edipotrebol <edipotrebol@gmail.com>
2014-05-19 19:26:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+EU CIBERACTION
+==============
+
+Este es el repositorio del sitio web de la FormAcción de #DaTactic2
---
+++ js/myscript.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	//ascensor
+$('#content').ascensor({
+	AscensorName:'house',
+	WindowsFocus:true,
+	WindowsOn:0,
+	
+	NavigationDirection:'xy',
+	Direction: 'x',
+	Navig:true,
+	Link:false,
+	ReturnURL:true,
+	PrevNext:false,
+	CSSstyles:true,
+	
+	KeyArrow:true,
+	keySwitch:true,
+	ReturnCode:false,
+	
+	ChocolateAscensor:true,
+	AscensorMap: '4|1',
+	ContentCoord: '1|1 & 1|2 & 1|3 & 1|4'
+});
+
+});
